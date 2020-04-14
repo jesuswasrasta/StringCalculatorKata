d73c09ef34f971b48ec4cabc809d792aadc2ab9f
Gianmarco Zecchini <gianmarcozecchini@gmail.com>
2020-04-14 10:47:14 +0200

    Added 2+3 test

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -11,31 +11,28 @@ require __DIR__ . '/../src/StringCalculator.php';
 final class StringCalculatorTest extends TestCase
 {
     private $calculator;
-
+    protected function setUp(): void
+    {
+        $this->calculator = new StringCalculator();
+    }
     public function testEmptyStringShouldReturnZero(): void
     {
-        $this->calculator = new StringCalculator();
-
         $result = $this->calculator->add('');
-
         $this->assertEquals(0, $result);
     }
-
     public function testIfIWillPassOneYouWillReturnOne(): void
     {
-        $this->calculator = new StringCalculator();
-
         $result = $this->calculator->add('1');
-
         $this->assertEquals(1, $result);
     }
-
     public function testIfIWillPassNumberYouWillReturnNumber(): void
     {
-        $this->calculator = new StringCalculator();
-
         $result = $this->calculator->add('12');
-
         $this->assertEquals(12, $result);
+    }
+    public function testIfIWillPassTwoPlusThreeItWillReturnFive(): void
+    {
+        $result = $this->calculator->add('2,3');
+        $this->assertEquals(5, $result);
     }
 }
